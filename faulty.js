68da65be10ed8be937678687d1f9dd5a508364bb
Mohini117 <141340470+Mohini117@users.noreply.github.com>
2023-12-02 20:58:29 +0530

    Exercise11
    #sigmawebdev

---
+++ faulty.js
@@ -0,0 +1,24 @@
+let  a=prompt("Enter first number:")
+let b=prompt("Enter second number:")
+let c=Math.random()<0.1
+if (c==true){
+    let add=a-b
+    let  mul=a+b
+    let sub=a/b
+    let div=a**b
+alert("addition of "+a+"and"+b+"is"+add)
+alert("multiplication of "+a+"and"+b+"is"+mul)
+alert("substraction of "+a+"and"+b+"is"+sub)
+alert("division of "+a+"and"+b+"is"+div) 
+}
+else{
+    let add=a+b
+    let  mul=a*b
+    let sub=a-b
+    let div=a/b
+
+alert("addition of "+a+"and"+b+"is"+add)
+alert("multiplication of "+a+"and"+b+"is"+mul)
+alert("substraction of "+a+"and"+b+"is"+sub)
+alert("division of "+a+"and"+b+"is"+div) 
+}
